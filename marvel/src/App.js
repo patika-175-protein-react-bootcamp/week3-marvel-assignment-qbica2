ace64142a568e4de888d290266017a10b4a57bd4
kubilay <kubidik27@gmail.com>
2022-03-31 15:50:08 +0300

    upgrade to react-18/ fetched data from api via axios

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,13 +1,43 @@
-import React from "react";
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React, { useState,useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const API_KEY = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
+  const HASH = process.env.REACT_APP_MARVEL_HASH;
+  const [cards,setCards]= useState([]);
+  const [pages,setPages] = useState(1);
+  const [totalPages,setTotalPages] = useState(10);
+
+  useEffect(()=>{
+    const getData= async ()=>{
+      const res= await axios(`https://gateway.marvel.com/v1/public/characters?apikey=${API_KEY}&hash=${HASH}&ts=1&offset=${(pages-1)*20}`);
+      setCards(res.data.data.results);
+      setTotalPages(res.data.data.total/20);
+      
+    };
+    getData();
+  },[pages]);
+  const array = [...Array(totalPages).keys()];
+  console.log(array.map(x=>x+1));
   return (
     <div className="App">
       <div className="header">
         <div className="logo">
           <img src="./image2.png" alt="logo" /> 
         </div>
+      </div>
+      <div className="cards">
+        {
+          cards.map(card=>(
+            <div className="card" key={card.id}>
+              <img  src={`${card.thumbnail.path}/portrait_incredible.${card.thumbnail.extension}`} alt={card.name} />
+              <div className="title">{card.name}</div>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
